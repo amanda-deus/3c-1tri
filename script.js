8dbab19a01c306d1a1fb89e854ddb3ff94362bfe
amanda <138232841+amanda-deus@users.noreply.github.com>
2025-04-24 20:42:36 +0000

    .

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
+document.addEventListener('DOMContentLoaded', function() {
+    const aumentaFonteBotao = document.getElementById('aumanta-fonte') ;
+    const diminuiFontebotao = document.getElementById('diminui-fonte') ;
+
+    let tamanhoAtualFonte = 1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
+    diminuiFontebotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
 
    const alternaContraste = document.getElementById('alterna-contraste')
    alternaContraste.addEventListener('click', function(){
