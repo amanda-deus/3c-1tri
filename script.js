325f05097452e11cbcc82905b73e29ad11ecdf10
amanda <138232841+amanda-deus@users.noreply.github.com>
2025-04-24 20:14:27 +0000

    .

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-    const aumentaFonteBotao = document.getElementById('auementar-fonte');
-    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
-
-    let tamanhoAtualFonte = 1;
-
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
-
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
-
-})
+botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+    opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+   })
 
  
     
