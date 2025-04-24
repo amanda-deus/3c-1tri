5d9cd021dbabfb236926657bf4a63b17e35a2322
amanda <138232841+amanda-deus@users.noreply.github.com>
2025-04-24 21:08:04 +0000

    .

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-botaoDeAcessibilidade.addEventListener('click', function (){
+botaoDeAcessibilidade.addEventListener('click', function () {
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
 
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() { 
     const aumentaFonteBotao = document.getElementById('aumenta-fonte'); 
     const diminuiFonteBotao = document.getElementById('diminui-fonte'); 
 
@@ -19,8 +19,4 @@ botaoDeAcessibilidade.addEventListener('click', function (){
             tamanhoAtualFonte -= 0.1;
             document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
         });
-});
-   const alternaContraste = document.getElementById('alterna-contraste')
-   alternaContraste.addEventListener('click', function(){
-    document.body.classList.toggle('alto-contraste')
-})
+    });
