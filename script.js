e498cd618f9c88473b86362d34ec3f98ea0b8e36
amanda <138232841+amanda-deus@users.noreply.github.com>
2025-04-24 20:27:01 +0000

    .

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
    })
 
- 
+   const alternaContraste = document.getElementById('alterna-contraste')
+   alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
     
